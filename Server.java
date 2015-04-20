7c4d17259f6e450c3c6547f96d818c8b171d1ec2
unknown <stefanm@LONWRK08546.eidos.com>
2015-04-20 14:55:53 +0100

    Added code to open a socket during the constructor.

--- Server.java
+++ Server.java
@@ -5,13 +5,14 @@ public class Server
 {
     private static final int PORT = 2000;
     
-    public Server() {
-        
+    private ServerSocket socket;
+    
+    public Server() throws IOException {
+        socket = new ServerSocket(PORT);
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Server server = new Server();
-        
         
     }
 }
