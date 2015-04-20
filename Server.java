33883fd92511b28b5c20273cce2fdb42ce1e0519
unknown <stefanm@LONWRK08546.eidos.com>
2015-04-20 14:51:55 +0100

    Created initial server class with fixed port.

---
+++ Server.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.*;
+
+public class Server
+{
+    private static final int PORT = 2000;
+    
+    public Server() {
+        
+    }
+    
+    public static void main(String[] args) {
+        Server server = new Server();
+        
+        
+    }
+}
