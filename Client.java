9910ba4616a95e1c1dab3f9979fef316714b1f7a
unknown <stefanm@LONWRK08546.eidos.com>
2015-04-20 14:51:41 +0100

    Created initial client class with address and port to server.

---
+++ Client.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.net.*;
+
+public class Client
+{
+    private static final String SERVER_IP = "127.0.0.1";
+    private static final int SERVER_PORT = 2000;
+    
+    public Client() {
+        
+    }
+    
+    public static void main(String[] args) {
+        Client client = new Client();
+        
+        
+    }
+}
