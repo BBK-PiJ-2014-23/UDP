9cda864fc02fc66a25be2b74a6be19379511e345
unknown <stefanm@LONWRK08546.eidos.com>
2015-04-20 15:16:22 +0100

    Implemented code to connect to a server.

--- Client.java
+++ Client.java
@@ -6,13 +6,20 @@ public class Client
     private static final String SERVER_IP = "127.0.0.1";
     private static final int SERVER_PORT = 2000;
     
+    private Socket socket;
+    
     public Client() {
         
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException, IOException {
         Client client = new Client();
-        
-        
+        client.connect();
+    }
+    
+    public void connect() throws UnknownHostException, IOException {
+        System.out.println("Connecting to server...");
+        socket = new Socket(SERVER_IP, SERVER_PORT);
+        System.out.println("Successfully connected via port " + socket.getPort());
     }
 }
