160a45f3d94428678c1ff314626c315685311494
unknown <stefanm@LONWRK08546.eidos.com>
2015-04-20 15:16:02 +0100

    Implemented method to listen and connecting clients.

--- Server.java
+++ Server.java
@@ -4,15 +4,22 @@ import java.net.*;
 public class Server
 {
     private static final int PORT = 2000;
-    
+
     private ServerSocket socket;
-    
+
     public Server() throws IOException {
         socket = new ServerSocket(PORT);
     }
-    
+
     public static void main(String[] args) throws IOException {
         Server server = new Server();
-        
+        while(true) {
+            server.listen();
+        }
+    }
+    public void listen() throws IOException {
+        System.out.println("Server listening on port " + socket.getLocalPort());
+        Socket client = socket.accept();
+        System.out.println("Client connected on port " + client.getPort());
     }
 }
