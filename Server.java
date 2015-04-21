5a4191755856e9342fc0c28514a2675a109f949e
unknown <stefanm@LONWRK08546.eidos.com>
2015-04-21 13:09:43 +0100

    Removed code that went into the server thread class. Added a test variable that stores the data coming from the sending client and which can be read and sent to other clients.

--- Server.java
+++ Server.java
@@ -8,16 +8,10 @@ public class Server
     private ServerSocket socket;
     private Socket client;
 
-    private DataInputStream inputStream;
-    private DataOutputStream outputStream;
-
-    private DatagramSocket datagramSocket;
-    private DatagramPacket packetFromClient;
-
     private int lastClientId;
     private boolean needsSender;
-
-    String test;
+    
+    private String test = "";
 
     public Server() throws IOException {
         socket = new ServerSocket(PORT);
@@ -29,55 +23,25 @@ public class Server
         Server server = new Server();
         while(true) {
             server.listen();
-            //server.setupStreams();
-            //server.tellClientId();
-            //server.tellClientRole();
-            //server.recieveData();
-            //server.sendData();
         }
     }
 
     public void listen() throws IOException {
-        System.out.println("Server listening on port " + socket.getLocalPort());
+        //System.out.println("Server listening on port " + socket.getLocalPort());
         client = socket.accept();
         System.out.println("Client connected on port " + client.getPort());
         lastClientId++;
-        (new ServerThread(client, lastClientId, needsSender)).start();
+        (new ServerThread(this, client, lastClientId, needsSender)).start();
         if (needsSender) {
             needsSender = false;
         }
     }
-
-    public void setupStreams() throws IOException {
-        inputStream = new DataInputStream(client.getInputStream());
-        outputStream = new DataOutputStream(client.getOutputStream());
-        datagramSocket = new DatagramSocket(2000);
+    
+    public void storeData(String input) {
+        test = input;
     }
-
-    public void tellClientId() throws IOException {
-        
-        System.out.println("Sending ID " + lastClientId + " to client");
-        outputStream.writeInt(lastClientId);
-    }
-
-    public void tellClientRole() throws IOException {
-        System.out.println("This needs a sender? " + needsSender);
-        outputStream.writeBoolean(needsSender);
-    }
-
-    public void recieveData() throws IOException {
-        byte[] data = new byte[2];
-        packetFromClient = new DatagramPacket(data, data.length);
-        datagramSocket.receive(packetFromClient);
-        test = new String(packetFromClient.getData());
-        System.out.println("Recieved test data from client " + test);
-    }
-
-    public void sendData() throws IOException {
-        InetAddress ip = packetFromClient.getAddress();
-        int port = packetFromClient.getPort();
-        byte[] data = test.getBytes();
-        DatagramPacket packetToClient = new DatagramPacket(data, data.length, ip, port);
-        datagramSocket.send(packetToClient);
+    
+    public String getData() {
+        return test;
     }
 }
